d446677416c491a057224acb6799e8e7b7487783
Praney Behl <praneybehl@gmail.com>
2015-11-05 22:05:10 +1100

    Update package dependencies

--- package.js
+++ package.js
@@ -1,19 +1,23 @@
 Package.describe({
   name: 'praneybehl:react-tooltip',
-  version: '0.0.1',
-  summary: '',
-  git: '',
+  version: '0.8.4',
+  summary: 'Simple Tooltip for React.js packaged for Meteor',
+  git: 'https://github.com/praneybehl/meteor-react-tooltip',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
+  api.use('react@0.1.13');
+  api.use('cosmos:browserify@0.8.3', 'client');
+  api.addFiles('package.browserify.js', 'client');
+  api.addFiles('package.browserify.options.json', 'client');
+  api.addFiles('react-tooltip.min.css', 'client');
+  api.export('ReactTooltip');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('praneybehl:react-tooltip');
-  api.addFiles('react-tooltip-tests.js');
+Npm.depends({
+  "react-tooltip": "0.8.4",
+  "exposify": "0.5.0"
 });
